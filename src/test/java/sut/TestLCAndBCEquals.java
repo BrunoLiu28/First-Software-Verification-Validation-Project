eca84bd7b59d2019cb5e40871c20b2a48675756f
Bruno Liu fc56297 <fc56297@alunos.fc.ul.pt>
2024-04-07 19:14:17 +0100

    desenvolvimento dos testes para o equals

--- src/test/java/sut/TestLCAndBCEquals.java
+++ src/test/java/sut/TestLCAndBCEquals.java
@@ -8,42 +8,33 @@ import java.util.List;
 
 import org.junit.Test;
 
-public class TestLCAndBCEquals {
+public class TestLCAndBCEquals { 
 	
-	//Line Coverage: 292
-	//Branch Coverage: B292
-//	@Test
-//	public void testSameObject() {
-//		List<Integer> list = Arrays.asList();  
-//		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 4);
-//		assertTrue(tree.equals(tree));
-//		
-//	}
-//	
-//	//Line Coverage: 292, 293
-//	//Branch Coverage: !B292, B293
-//	@Test
-//	public void testDifferentInstances() {
-//		List<Integer> list = Arrays.asList();  
-//		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 4);
-//		assertFalse(tree.equals(list));
-//		
-//	}
-//	
-//	//Line Coverage: 292, 293, 294, 300, 303, 304, 305, 307, 308, 311, 312
-//	//Branch Coverage: !B292, !B293, B294, !B300, !B303(1), !B303(2),
-//	//	!B307(1), !B307(2), B307(1), B307(2) !B308, B311(1), B311(2)
-//	@Test
-//	public void test2() {
-//		List<Integer> list = Arrays.asList(1);  
-//		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 4);
-//		List<Integer> list2 = Arrays.asList(1);  
-//		ArrayNTree<Integer> tree2 = new ArrayNTree<>(list2, 4);
-//		assertTrue(tree.equals(tree2));
-//	}
+	@Test
+	public void testSameObject() {
+		List<Integer> list = Arrays.asList();  
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 4);
+		assertTrue(tree.equals(tree));
+		
+	}
 	
-	//Line Coverage: 292, 293, 294
-	//Branch Coverage: !B292, !B293, 
+	@Test
+	public void testDifferentInstances() {
+		List<Integer> list = Arrays.asList();  
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 4);
+		assertFalse(tree.equals(list));
+		
+	}
+	
+	@Test
+	public void test2() {
+		List<Integer> list = Arrays.asList(1);  
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 4);
+		List<Integer> list2 = Arrays.asList(1);  
+		ArrayNTree<Integer> tree2 = new ArrayNTree<>(list2, 4);
+		assertTrue(tree.equals(tree2));
+	}
+	
 	@Test
 	public void test4() {
 		List<Integer> list = Arrays.asList(1,2);
@@ -53,15 +44,55 @@ public class TestLCAndBCEquals {
 		assertTrue(tree.equals(tree2));
 	}
 	
-	//Line Coverage: 292, 293, 294
-	//Branch Coverage: !B292, !B293, 
-//	@Test
-//	public void test3() {
-//		List<Integer> list = Arrays.asList(1,2);  
-//		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 4);
-//		List<Integer> list2 = Arrays.asList(1);  
-//		ArrayNTree<Integer> tree2 = new ArrayNTree<>(list2, 4);
-//		assertFalse(tree.equals(tree2));
-//	}
+	@Test
+	public void test3() {
+		List<Integer> list = Arrays.asList(1,2);  
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 4);
+		List<Integer> list2 = Arrays.asList(1);  
+		ArrayNTree<Integer> tree2 = new ArrayNTree<>(list2, 4);
+		assertFalse(tree.equals(tree2));
+	}
 	
+	@Test
+	public void test5() {
+		List<Integer> list = Arrays.asList(2);  
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 4);
+		List<Integer> list2 = Arrays.asList(2,1);  
+		ArrayNTree<Integer> tree2 = new ArrayNTree<>(list2, 4);
+		assertFalse(tree.equals(tree2));
+	}
+	
+	@Test
+	public void test6() {
+		List<Integer> list = Arrays.asList(1);  
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 4);
+		List<Integer> list2 = Arrays.asList();  
+		ArrayNTree<Integer> tree2 = new ArrayNTree<>(list2, 4);
+		assertFalse(tree.equals(tree2));
+	}
+	
+	@Test
+	public void test7() {
+		List<Integer> list = Arrays.asList(1,2,3,4,5,6);  
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 2);
+		List<Integer> list2 = Arrays.asList(1,2,3,4,5);  
+		ArrayNTree<Integer> tree2 = new ArrayNTree<>(list2, 2);
+		assertFalse(tree.equals(tree2));
+	}
+	
+	@Test
+	public void test8() {
+		List<Integer> list = Arrays.asList(1,2,3,4,5);  
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 2);
+		List<Integer> list2 = Arrays.asList(1,2,3,4,5,6);  
+		ArrayNTree<Integer> tree2 = new ArrayNTree<>(list2, 2);
+		assertFalse(tree.equals(tree2));
+	}
+	
+	@Test
+	public void test9() {
+		List<Integer> list = Arrays.asList();  
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 3);
+		assertFalse(tree.equals((ArrayNTree<Integer>)null));
+	}
 }
