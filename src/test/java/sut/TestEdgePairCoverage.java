8a3d434e13d382dd25120f5a57ec96d1dfb4dea5
Bruno Liu fc56297 <fc56297@alunos.fc.ul.pt>
2024-04-15 19:41:45 +0100

    bora

--- src/test/java/sut/TestEdgePairCoverage.java
+++ src/test/java/sut/TestEdgePairCoverage.java
@@ -1,18 +1,68 @@
 package sut;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
 public class TestEdgePairCoverage {
 
 //	ONE == OTHER  [1,2]
-	
+	@Test
+	public void testSameObject() {
+		List<Integer> list = Arrays.asList(1,2);
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 4);
+		assertTrue(tree.equals(tree));
+	}
 //	ONE == NULL || OTHER == NULL  [1,3,4]
+	@Test
+	public void testOneNull() {
+		List<Integer> list = Arrays.asList(1,2);
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 4);
+		assertFalse(tree.equals(null));
+	}
 
 //	SE UM TEM NEXT E OUTRO NAO [1,3,5,6,7,8,4]
+	@Test
+	public void testOneDontHaveNext() {
+		List<Integer> list = Arrays.asList(1);
+		List<Integer> list2 = Arrays.asList();
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 4);
+		ArrayNTree<Integer> tree2 = new ArrayNTree<>(list2, 4);
+		assertFalse(tree.equals(tree2));
+	}
 	
 //	AMBOS NAO TEM NEXT- SAO EQUALS [1,3,5,6,7,8,9]
+	@Test
+	public void testEqualTrees() {
+		List<Integer> list = Arrays.asList(1,2,4,5,6);
+		List<Integer> list2 = Arrays.asList(1,2,4,5,6);
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 4);
+		ArrayNTree<Integer> tree2 = new ArrayNTree<>(list2, 4);
+		assertTrue(tree.equals(tree2));
+	}
 	
 //	TEM 1 ELEMENTO NO MINIMO EM CADA LISTA E NUMA DELAS NAO TEM SEGUNDO [1,3,5,6,7,10,7,8,4]
+	@Test
+	public void testOneHasMoreThanOther() {
+		List<Integer> list = Arrays.asList(1,2);
+		List<Integer> list2 = Arrays.asList(1);
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 4);
+		ArrayNTree<Integer> tree2 = new ArrayNTree<>(list2, 4);
+		assertFalse(tree.equals(tree2));
+	}
 	
 //	 TEM 1 ELEMENTO E ESSE PRIMEIRO É DIFERENTE  [1,3,5,6,7,10,7,10,11]
-	
+	@Test
+	public void test4() {
+		List<Integer> list = Arrays.asList(1,2);
+		List<Integer> list2 = Arrays.asList(1,3);
+		ArrayNTree<Integer> tree = new ArrayNTree<>(list, 4);
+		ArrayNTree<Integer> tree2 = new ArrayNTree<>(list2, 4);
+		assertFalse(tree.equals(tree2));
+	}
 	
 }
